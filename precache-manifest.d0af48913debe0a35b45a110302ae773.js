4e0275d16e7ba2a82555294ce324ce7fe11b2505
Mikolaj Adam Manczak <mikomanczak@gmail.com>
2018-11-04 17:05:59 +0100

    Updates

---
+++ precache-manifest.d0af48913debe0a35b45a110302ae773.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "241adc774da6e73249dd",
+    "url": "/Sync-in/static/js/runtime~main.241adc77.js"
+  },
+  {
+    "revision": "cb32808a727b6c3340ee",
+    "url": "/Sync-in/static/js/main.cb32808a.chunk.js"
+  },
+  {
+    "revision": "d3eb1eef31ff88d92d46",
+    "url": "/Sync-in/static/js/1.d3eb1eef.chunk.js"
+  },
+  {
+    "revision": "cb32808a727b6c3340ee",
+    "url": "/Sync-in/static/css/main.40dc8ea3.chunk.css"
+  },
+  {
+    "revision": "ebf51e52072e2048a1c346a8d67d9c9e",
+    "url": "/Sync-in/index.html"
+  }
+];
